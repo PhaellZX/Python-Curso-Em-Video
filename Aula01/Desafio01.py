91fa0db0dbf18a82cdf10d022f5975844db6ec57
Raphael <48337836+PhaellZX@users.noreply.github.com>
2021-10-20 22:22:06 -0300

    Add files via upload

---
+++ Aula01/Desafio01.py
@@ -0,0 +1,9 @@
+## DESAFIO 01 ##
+# Crie um script Python que leia #
+# o nome de uma pessoa e mostre  #
+# uma mensagem de boas-vindas de #
+# acordo com valor digitado #
+
+print('=== DESAFIO 01 ===')
+nome = input('Digite o seu nome: ')
+print('Seja Bem-vindo(a) ' + nome + ', prazer em te conhecer!')
