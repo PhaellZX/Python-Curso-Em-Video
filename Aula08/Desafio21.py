95aec6d1dfebdf2d35ac400c3c2c66689237f86f
Raphael <48337836+PhaellZX@users.noreply.github.com>
2021-11-08 15:06:43 -0300

    Add files via upload

---
+++ Aula08/Desafio21.py
@@ -0,0 +1,11 @@
+# DESAFIO 21 #
+#   Faça um programa em Python que abra e reproduza
+#   o áudio de um arquivo MP3.
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('musica.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+
+# Não está funcionando, parece que falta um arquivo especifico no modulo :/
