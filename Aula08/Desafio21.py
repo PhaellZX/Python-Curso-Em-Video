309bb6cf2f56e70650b4bbffc9a0d37f26303fb8
Raphael <48337836+PhaellZX@users.noreply.github.com>
2021-11-09 14:17:31 -0300

    Add files via upload

--- Aula08/Desafio21.py
+++ Aula08/Desafio21.py
@@ -2,10 +2,16 @@
 #   Faça um programa em Python que abra e reproduza
 #   o áudio de um arquivo MP3.
 
-import pygame
-pygame.init()
-pygame.mixer.music.load('musica.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
+# Esse código funcionou :)
+import playsound
+input('PLAY ♫ (Pressione o enter)')
+print('Musica Rodando... ♫♪')
+playsound.playsound('musica.mp3')
+print('acabou...')
 
-# Não está funcionando, parece que falta um arquivo especifico no modulo :/
+# Esse não, não está carregando a dll libmpg123-0 :\
+#import pygame
+#pygame.init()
+#pygame.mixer.music.load('musica.mp3')
+#pygame.mixer.music.play()
+#pygame.event.wait()
