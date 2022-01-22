a9c014f6176df63ce67cae3cbb5646ba88ff8758
Raphael <48337836+PhaellZX@users.noreply.github.com>
2022-01-21 21:12:18 -0300

    Delete Aula06 directory

--- Aula06/Desafio01.py
+++
@@ -1,8 +0,0 @@
-# DESAFIO 01 #
-# Crie um programa que leiaa dois #
-# E mostre a soma entre eles #
-
-n1 = int(input('Digite um numero '))
-n2 = int(input('Digite outro numero '))
-s = n1 + n2
-print('A soma de {} + {} = {}'.format(n1,n2,s))
