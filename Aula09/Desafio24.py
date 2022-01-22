a4c2da4c511069575745a5123657b9079cbb8d4f
Raphael <48337836+PhaellZX@users.noreply.github.com>
2022-01-21 21:13:26 -0300

    Delete Aula09 directory

--- Aula09/Desafio24.py
+++
@@ -1,7 +0,0 @@
-# DESAFIO 24 #
-# Crie um programa que leia o nome de uma cidade
-# diga se ela começa ou não com o nome "SANTO".
-
-cidade = input('Em que cidade você nasceu? ').strip()
-print(cidade[:5].upper() == 'SANTO')
-
--- Aula09/Desafio25.py
+++
@@ -1,6 +0,0 @@
-# DESAFIO 25 #
-#  Crie um programa que leia o nome
-#  de uma pessoa e diga se ela tem "SILVA" no nome.
-
-nome = input('Qual é seu nome completo? ').strip()
-print('Seu nome tem Silva? {}'.format('Silva' in nome.lower()))
