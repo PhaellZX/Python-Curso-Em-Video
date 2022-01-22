72916583b1019e28375435a515a39c8d70eae493
Raphael <48337836+PhaellZX@users.noreply.github.com>
2022-01-21 21:15:00 -0300

    Delete CoresNoPython directory

--- CoresNoPython/TesteCores.py
+++
@@ -1,6 +0,0 @@
-nome = 'Raphael'
-cores = {'limpa':'\033[m',
-         'azul':'\033[34m',
-         'amarelo':'\033[33m',
-         'pretobranco':'\033[7;30m'}
-print('Olá! Muito prazer em te conhecer, {}{}{}!!'.format(cores['pretobranco'],nome,cores['limpa']))
