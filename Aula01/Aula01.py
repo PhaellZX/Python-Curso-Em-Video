46352ad4ec579571694b0b195f84cfc197307404
Raphael <48337836+PhaellZX@users.noreply.github.com>
2022-01-21 21:11:58 -0300

    Delete Aula01 directory

--- Aula01/Aula01.py
+++
@@ -1,9 +0,0 @@
-## AULA 01 ##
-
-## input() -> entrada de dados no teclado ##
-## print() -> escrever na tela ##
-
-nome = input('Qual é o seu nome?')
-idade = input('Qual é sua idade?')
-peso = input('Qual é o seu peso?')
-print(nome, idade, peso)
--- Aula01/Desafio03.py
+++
@@ -1,9 +0,0 @@
-## DESAFIO 03 ##
-# Crie um script Python que leia dois números #
-# e tente mostrar a soma entre eles #
-
-print('=== DESAFIO 03 ===')
-num1 = input('Digite o primeiro número: ')
-num2 = input('Digite o segundo número: ')
-soma = int(num1) + int(num2) ## int() -> Converter para int ##
-print("Resultado: " + num1 + " + " + num2 +" = " + str(soma)) ## str() -> Converter para String ## 
