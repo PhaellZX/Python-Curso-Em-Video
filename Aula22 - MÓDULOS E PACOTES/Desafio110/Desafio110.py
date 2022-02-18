4a7754fe85c0492981a6f97447a77534a7526453
Raphael <48337836+PhaellZX@users.noreply.github.com>
2022-02-18 11:08:06 -0300

    Add files via upload

---
+++ Aula22 - MÓDULOS E PACOTES/Desafio110/Desafio110.py
@@ -0,0 +1,11 @@
+''' DESAFIO 110
+Adicione o módulo moeda.py criado nos desafios anteriores,
+uma função chamada resumo(), que mostre na tela algumas
+informações geradas pelas funções que já temos no módulo
+criado até aqui.
+'''
+import Moeda
+
+p = float(input('Digite o preço: R$'))
+Moeda.resumo(p, 20, 12)
+
