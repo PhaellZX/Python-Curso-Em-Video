4daa7bee049cb5dee54bd32cc2fbd1f7c9d9f487
Raphael <48337836+PhaellZX@users.noreply.github.com>
2022-02-22 12:57:58 -0300

    Add files via upload

---
+++ Aula23 - EXCEÇÕES DE ERROS/Desafio113.py
@@ -0,0 +1,36 @@
+''' DESAFIO 113
+Reescreva a função leiaInt() que fizemos no desafio 104, incluindo agora a
+possibilidade da digitação de um número de tipo inválido. Aproveite e crie
+também uma função leiaFloat() com a mesma funcionalidade.
+'''
+def leiaInt(msg):
+        while True:
+            try:
+                n = int(input(msg))
+            except (ValueError, TypeError):
+                print('\033[31mERRO: por favor, digite um número inteiro válido. \033[m')
+                continue
+            except (KeyboardInterrupt):
+                print('\n\033[31mpor favor, Entrada de dados interropida pelo usário. \033[m')
+                return 0
+            else:
+                return n
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: por favor, digite um número real válido. \033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mpor favor, Entrada de dados interropida pelo usário. \033[m')
+            return 0
+        else:
+            return n
+
+
+n1 = leiaInt('Digite um valor inteiro: ')
+n2 = leiaFloat('Digite um valor real: ')
+print(f'O valor inteiro foi {n1} e o valor real foi {n2}')
